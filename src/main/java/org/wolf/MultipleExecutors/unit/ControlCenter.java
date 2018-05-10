e2e3e1693226a8cc64b50e6603982d3daa4c5ccf
Volkov Danil <Linad29@mail.ru>
2018-05-10 16:45:25 +0500

    Generate executors into ControlCenter

--- src/main/java/org/wolf/MultipleExecutors/unit/ControlCenter.java
+++ src/main/java/org/wolf/MultipleExecutors/unit/ControlCenter.java
@@ -1,26 +1,64 @@
 package org.wolf.MultipleExecutors.unit;
 
+import org.wolf.MultipleExecutors.Cell;
 import org.wolf.MultipleExecutors.Game;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ControlCenter
 {
-	Executor[] explorers;
-	Executor[] harvesters;
-	Game game;
+	private int x;
+	private int y;
 
-	public ControlCenter(Game game, int countExplorer, int countHarvester)
+	private Executor[] explorers;
+	private Executor[] harvesters;
+	private Game game;
+
+	public Executor[] getExplorers()
 	{
+		return explorers;
+	}
+
+	public ControlCenter(int x, int y, Game game, int countExplorer, int countHarvester)
+	{
+		this.x = x;
+		this.y = y;
 		this.game = game;
 		this.explorers = new Explorer[countExplorer];
 		this.harvesters = new Harvester[countHarvester];
+		game.map[x][y] = Cell.Center;
+
+		ArrayList<Cell> allowCell = new ArrayList<>();
+		allowCell.add(Cell.Ground);
 
 		Random random = new Random();
-		for(int i = 0; i<countExplorer; i++) {
-			explorers[i] = new Explorer(1,2);
+		for (int i = 0; i < countExplorer; i++) {
+			int unitX = x;
+			int unitY = y;
+
+			while (!allowCell.contains(game.map[unitX][unitY])) {
+				unitX = random.nextInt(5) + this.x;
+				unitY = random.nextInt(5) + this.y;
+			}
+			explorers[i] = new Explorer(unitX, unitY, game);
+		}
+
+		for (int i = 0; i < countHarvester; i++) {
+			harvesters[i] = new Harvester(this.x, this.y, game);
 		}
 	}
+
+	public int getExplorersCount()
+	{
+		return explorers.length;
+	}
+
+	public int getHarvesterCount()
+	{
+		return harvesters.length;
+	}
+
 
 	public void step()
 	{
