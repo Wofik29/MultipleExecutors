7c893f225c1a322d628198775724ef8a7bcbff7d
Volkov Danil <Linad29@mail.ru>
2018-05-10 23:22:01 +0500

    Fix on access level field in canvas and color stroke

--- src/main/java/org/wolf/MultipleExecutors/Canvas.java
+++ src/main/java/org/wolf/MultipleExecutors/Canvas.java
@@ -7,7 +7,7 @@ public class Canvas extends javafx.scene.canvas.Canvas
 {
 
 	private Cell[][] map;
-	private int widthCell = 15;
+	public int widthCell = 15;
 	public int shiftLeft;
 	public int shiftUp;
 
@@ -35,12 +35,13 @@ public class Canvas extends javafx.scene.canvas.Canvas
 			return;
 		}
 
-		getGraphicsContext2D().setLineWidth(2);
+		getGraphicsContext2D().setLineWidth(1);
 		for (int x = 0; x < map.length; x++) {
 			for (int y = 0; y < map[x].length; y++) {
-				getGraphicsContext2D().setStroke(map[x][y].color);
+				getGraphicsContext2D().setStroke(Color.BLACK);
 				getGraphicsContext2D().setFill(map[x][y].color);
 				getGraphicsContext2D().fillRect(x * widthCell + shiftLeft, y * widthCell + shiftUp, widthCell, widthCell);
+				getGraphicsContext2D().strokeRect(x * widthCell + shiftLeft, y * widthCell + shiftUp, widthCell, widthCell);
 			}
 		}
 
