f1390e5e10d7ec5d3165cb791bb7bc6a28e2ad81
Volkov Danil <Linad29@mail.ru>
2018-05-10 13:15:32 +0500

    Update Explorer and Harvester

--- src/main/java/org/wolf/MultipleExecutors/unit/Explorer.java
+++ src/main/java/org/wolf/MultipleExecutors/unit/Explorer.java
@@ -1,9 +1,11 @@
 package org.wolf.MultipleExecutors.unit;
 
+import org.wolf.MultipleExecutors.Game;
+
 public class Explorer extends Unit
 {
-	public Explorer(int x, int y)
+	public Explorer(int x, int y, Game game)
 	{
-		super(x, y);
+		super(x, y, game);
 	}
 }
--- src/main/java/org/wolf/MultipleExecutors/unit/Harvester.java
+++ src/main/java/org/wolf/MultipleExecutors/unit/Harvester.java
@@ -1,9 +1,11 @@
 package org.wolf.MultipleExecutors.unit;
 
+import org.wolf.MultipleExecutors.Game;
+
 public class Harvester extends Unit
 {
-	public Harvester(int x, int y)
+	public Harvester(int x, int y, Game game)
 	{
-		super(x, y);
+		super(x, y, game);
 	}
 }
