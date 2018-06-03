330dbf5a3b6397a81dc7665c07022a5433e53606
Volkov Danil <Linad29@mail.ru>
2018-06-03 22:25:22 +0500

    Message and change state game on editor stage

--- src/main/java/org/wolf/MultipleExecutors/controllers/EditorController.java
+++ src/main/java/org/wolf/MultipleExecutors/controllers/EditorController.java
@@ -1,8 +1,10 @@
 package org.wolf.MultipleExecutors.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import org.wolf.MultipleExecutors.Game;
+import org.wolf.MultipleExecutors.State;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -10,7 +12,30 @@ import java.util.Observer;
 public class EditorController implements Observer
 {
 	@FXML
-	private TextArea explorer;
+	public TextArea explorer;
+
+	@FXML
+	public Label message;
+
+	private Game game;
+
+	@FXML
+	private void initialize()
+	{
+
+	}
+
+	public void setGame(Game game)
+	{
+		this.game = game;
+		game.setEditorController(this);
+	}
+
+	@FXML
+	public void onStart()
+	{
+		game.changeState(State.Play);
+	}
 
 	@Override
 	public void update(Observable o, Object arg)
@@ -24,8 +49,12 @@ public class EditorController implements Observer
 			case Welcome:
 			case Pause:
 			case Play:
-				System.out.println("UPDATE");
 				explorer.setDisable(true);
 		}
+	}
+
+	public void setMessage(String message)
+	{
+		this.message.setText(message);
 	}
 }
